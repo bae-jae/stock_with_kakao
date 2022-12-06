7fc2020d4eb504bdcfdd51a728a5191bd2b91b73
timo <timo@clobot.co.kr>
2022-12-06 22:01:20 +0900

    [model] Create models
    - 매번 모든 테마와 종목을 크롤링하는 것은 비효율 적임
    - 크론탭을 이용해 장전에 크롤링하도록 변경함
    
    related to #6

--- djangostock/stocks/models.py
+++ djangostock/stocks/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+class StockInfo(models.Model):
+    stock_name = models.CharField(primary_key=True, max_length=100)
+    stock_cap = models.CharField(max_length=100)
+    themas = models.CharField(max_length=300)
 
-# Create your models here.
+    def __str__(self) -> str:
+        return  self.stock_name
